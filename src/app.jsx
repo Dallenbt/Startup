0c05af8834e6f07f549202c6b296f18c91fd0820
Dallen <dallenafish11@gmail.com>
2026-02-09 19:50:10 -0700

    got my header and footer in react

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+        <header>
+            <h1 className="banner">BiteFight</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="play.html">Play</a></li>
+                    <li><a href="results.html">Results</a></li>
+                </ul>
+            </nav>
+        </header>
+        <hr/>
+        
+        <main>
+            <p>componet here</p>
+        </main>
+
+        <footer>
+        <hr />
+        <span>Dallen Thompson</span>
+        <br />
+        <a href="https://github.com/Dallenbt/Startup" target="_blank">GitHub</a>
+        </footer>
+    </div>;
 }
