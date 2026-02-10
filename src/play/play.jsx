7b6028580a25f01f4ec4e5ca6f093188890d4653
Dallen <dallenafish11@gmail.com>
2026-02-09 20:58:10 -0700

    added play page

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>play displayed here</div>
+    <main>
+        <h1>Time to Fight!</h1>
+        <p className = "alert alert-info notif" >Dallen voted for Corndog</p>
+        <main className="play-main">
+        <article className ="food-card">
+        <h3>Corndog</h3>
+        <p>Corndog Votes: 10</p>
+        <img src="/corndog.jpg" alt="Corndog"></img>
+        <button className="btn btn-dark voter">Vote Corndog</button>
+        </article>
+        <h1 className="vs" id="playVS">VS</h1>
+        <article className ="food-card">
+        <h3>Hotdog</h3>
+        <p>Hotdog Votes: 8</p>
+        <img src="/hotdog.jpg" alt="Hotdog"></img>
+        <button className="btn btn-dark voter">Vote Hotdog</button>
+        </article>
+    </main>
     </main>
   );
 }
