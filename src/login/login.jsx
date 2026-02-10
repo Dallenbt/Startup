df4581cac51b6d39e81698a45a7d3f8631fba08f
Dallen <dallenafish11@gmail.com>
2026-02-09 20:21:51 -0700

    added login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+        <h1>Welcome to BiteFight</h1>
+      <form method="get" action="play.html">
+        <div>
+          <span>Name</span>
+          <input type="text" placeholder="Your name" />
+        </div>
+        <button type="submit" className="btn btn-dark" id="join">Join the fight</button>
+      </form>
     </main>
   );
 }
