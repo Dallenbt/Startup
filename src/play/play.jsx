ad9b3af84bf63c928d952c1098f617c5f88274bf
Dallen <dallenafish11@gmail.com>
2026-02-27 11:11:27 -0700

    worked on voter page

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -6,7 +6,10 @@ export function Play({user}) {
   const [food2, setFood2] = React.useState('');
   const [vote1, setVote1] = React.useState(0);
   const [vote2, setVote2] = React.useState(0);
-  const [timeLeft, setTimeLeft] = React.useState(20);
+  const [timeLeft, setTimeLeft] = React.useState(10);
+  const [notification, setNotification] = React.useState('');
+  const [winner, setWinner] = React.useState('');
+  const [loser, setLoser] = React.useState('');
 
   if (!user) {
     user = {Name: 'Guest'};
@@ -30,7 +33,7 @@ export function Play({user}) {
           setVote1(0);
           setVote2(0);
           fetchFoodPair();
-          return 20;
+          return 10;
         }
         return prev - 1;
       });
@@ -39,24 +42,37 @@ export function Play({user}) {
     return () => clearInterval(timer);
   }, []);
 
+  React.useEffect(() => {
+    if (vote1 > vote2 && vote1 > 0) {
+      setWinner(food1);
+      setLoser(food2);
+    } else if (vote2 > vote1 && vote2 > 0) {
+      setWinner(food2);
+      setLoser(food1);
+    }
+  }, [vote1, vote2]);
+
+  
+
   return (
     <main>
         <h1>Time to Fight!</h1>
+        <p className="alert alert-success notif">Winner: {winner}</p>
         <h1>Time left to vote: {timeLeft}</h1>
-        <p className = "alert alert-info notif" > {user.Name} voted for {food1}</p>
+        <p className = "alert alert-info notif" >{notification}</p>
         <main className="play-main">
         <article className ="food-card">
         <h3>{food1}</h3>
         <p>{food1} Votes: {vote1}</p>
         <img src="/glove-removebg-preview.png" alt={food1} className="food-image"></img>
-        <button className="btn btn-dark voter" onClick={() => setVote1(vote1 + 1)}>Vote {food1}</button>
+        <button className="btn btn-dark voter" onClick={() => {setVote1(vote1 + 1); setNotification(`${user.Name} voted for ${food1}`);}}>Vote {food1}</button>
         </article>
         <h1 className="vs" id="playVS">VS</h1>
         <article className ="food-card">
         <h3>{food2}</h3>
         <p>{food2} Votes: {vote2}</p>
         <img src="/glove-removebg-preview.png" alt={food2} className="food-image" id="right-image"></img>
-        <button className="btn btn-dark voter" onClick={() => setVote2(vote2 + 1)}>Vote {food2}</button>
+        <button className="btn btn-dark voter" onClick={() => {setVote2(vote2 + 1); setNotification(`${user.Name} voted for ${food2}`);}}>Vote {food2}</button>
         </article>
     </main>
     </main>
