97656422bde8842df4727107a1e904cf21d191a9
Dallen <dallenafish11@gmail.com>
2026-03-21 11:28:40 -0600

    worked on database

--- service/database.js
+++ service/database.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
-const url = `mongodb://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const scoreCollection = db.collection('scores');
 
 (async function testConnection() {
   try {
