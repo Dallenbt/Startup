ad4e5db64288c7dce4cbbb66371b5850f122630e
Dallen <dallenafish11@gmail.com>
2026-03-21 10:37:52 -0600

    added dbconfig

--- service/index.js
+++ service/index.js
@@ -1,9 +1,9 @@
-
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
